1c2ae952af7d6d5c563b72796d9ba672d97ef175
Jamz <JazenaYLA@users.noreply.github.com>
2026-01-25 16:10:51 -0600

    Needs to have Cassandra

--- thehive4-cortex4-n8n/prepare-vol-data.sh
+++ thehive4-cortex4-n8n/prepare-vol-data.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 
 sudo m# Create dirs + configs
-mkdir -p vol/{cortex,thehive,postgres,n8n}
-sudo chown -R 1000:1000 vol/{cortex,thehive,postgres,n8n}
+mkdir -p vol/{cassandra/data,thehive,cortex,postgres,n8n}
+sudo chown -R 1000:1000 vol/{cassandra/data,thehive,cortex,postgres,n8n}
