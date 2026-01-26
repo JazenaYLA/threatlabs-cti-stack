d885b3acd59b1e749000c807b28e9d3b5b0b5f19
Jamz <JazenaYLA@users.noreply.github.com>
2026-01-26 00:51:50 -0600

    Updates to es versions

--- thehive4-cortex4-n8n/create-cortex-index.sh
+++ thehive4-cortex4-n8n/create-cortex-index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run --rm --network=cti-net curlimages/curl \
-  -X PUT 'http://es-cti:9200/cortex_6' \
+  -X PUT 'http://es8-cti:9200/cortex_6' \
   -H 'Content-Type: application/json' \
   -d '{"mappings":{"properties":{"name":{"type":"keyword"},"description":{"type":"text"},"dockerImage":{"type":"keyword"},"dockerVersion":{"type":"keyword"},"dataType":{"type":"keyword"},"tlp":{"type":"float"},"pap":{"type":"float"}}}}'
