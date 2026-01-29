673a3152c4237c3d0805c3447b340f3e2ac610ef
Jamz <JazenaYLA@users.noreply.github.com>
2026-01-29 00:20:19 -0600

    remove legacy folders

--- thehive4-cortex4-n8n/prepare-vol-data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-
-# Create dirs + configs
-sudo mkdir -p vol/{cassandra/data,thehive,cortex,postgres/data,n8n}
-sudo chown -R 1000:1000 vol/{cassandra,thehive,cortex,postgres,n8n}
