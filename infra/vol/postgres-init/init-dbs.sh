6c9113a311cc7f1f8cc434dabc0b2dbb0784809d
Jamz <JazenaYLA@users.noreply.github.com>
2026-02-03 22:02:44 -0600

    update env variables

--- infra/vol/postgres-init/init-dbs.sh
+++ infra/vol/postgres-init/init-dbs.sh
@@ -25,12 +25,12 @@ echo "XXX Creating Additional Databases XXX"
 
 # OpenAEV / OpenCTI
 # Note: OpenCTI often uses Elastic, but OpenAEV needs Postgres.
-create_db_and_user "openaev" "openaev" "changeme"
+create_db_and_user "openaev" "openaev" "${OPENAEV_DB_PASSWORD:-changeme}"
 
 # n8n
-create_db_and_user "n8n" "n8n" "n8npass123!"
+create_db_and_user "n8n" "n8n" "${N8N_DB_PASSWORD:-n8npass123!}"
 
 # FlowIntel
-create_db_and_user "flowintel" "flowintel" "changeme"
+create_db_and_user "flowintel" "flowintel" "${FLOWINTEL_DB_PASSWORD:-changeme}"
 
 echo "XXX Databases created XXX"
